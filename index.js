7bc2c83410d909bb532186c76ff63287ebed1cc1
Damian Bravo <Dbravo1005@gmail.com>
2023-09-17 22:33:52 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData() {
+    const userData = {
+      name: "Steve",
+      email: "steve@steve.com"
+    };
+  
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+  }
+  
